66986b2fc5dd08d36b72af55f405ea00a8723a92
Kurt M. Weber <kurt.maxwell.weber@gmail.com>
2025-11-18 20:12:32 -0600

    makefiles for tools

---
+++ boot/bootloader.c
@@ -0,0 +1,20 @@
+/* Creator: Kurt M. Weber
+ *
+ * Created on: 2025-11-18
+ *
+ * This file is part of CosmOS, a component of the Cosmoverse.
+ *
+ * Licensed under Hippocratic License with clauses:
+ * HL3-CL-ECO-EXTR-FFD-MEDIA-MY-SUP-SV-TAL-USTA-XUAR See file LICENSE for full
+ * licensing information.
+ */
+
+#include <efi/efi.h>
+#include <efi/efilib.h>
+
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle,
+                           EFI_SYSTEM_TABLE *SystemTable) {
+  InitializeLib(ImageHandle, SystemTable);
+  Print(L"Hello, world!\n");
+  return EFI_SUCCESS;
+}
