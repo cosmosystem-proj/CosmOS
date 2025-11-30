bc5ad5cbf89777c6351b3905fd3dfdd2f1ed9555
Kurt M. Weber <kurt.maxwell.weber@gmail.com>
2025-11-30 15:40:04 -0600

    split loading file up into multiple functions for readability and cleanliness

--- boot/loadkernel.c
+++ boot/loadkernel.c
@@ -15,29 +15,35 @@
 #include <boot/convenience.h>
 #include <boot/error.h>
 
-void load_kernel(EFI_HANDLE image_handle) {
-  const EFI_GUID prot_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
-  EFI_LOADED_IMAGE *loaded_image;
-  EFI_FILE_HANDLE volume_handle;
+EFI_STATUS get_loaded_image(EFI_HANDLE image_handle,
+                            EFI_LOADED_IMAGE **loaded_image) {
   EFI_STATUS status;
+  const EFI_GUID prot_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+  EFI_LOADED_IMAGE *tmp = 0;
 
   status = uefi_call_wrapper(BS->HandleProtocol, 3, image_handle, &prot_guid,
-                             (void **)&loaded_image);
+                             (VOID **)&tmp);
 
-  if (status != EFI_SUCCESS) {
-    Print(L"Obtaining image handle failed\r\n");
-    wait_key_press();
-    return;
-  }
+  *loaded_image = tmp;
+
+  return status;
+}
+
+EFI_FILE_HANDLE get_volume_handle(EFI_LOADED_IMAGE *loaded_image) {
+  EFI_FILE_HANDLE volume_handle;
 
   volume_handle = LibOpenRoot(loaded_image->DeviceHandle);
   if (!volume_handle) {
     Print(L"Opening device root failed\r\n");
     wait_key_press();
-    return;
+    return NULL;
   }
 
-  CHAR16 *path = u"\\EFI\\BOOT\\test.txt";
+  return volume_handle;
+}
+
+EFI_FILE_HANDLE open_file(EFI_FILE_HANDLE volume_handle, CHAR16 *path) {
+  EFI_STATUS status;
   EFI_FILE_HANDLE file_handle;
   status =
       uefi_call_wrapper(volume_handle->Open, 5, volume_handle, &file_handle,
@@ -46,14 +52,43 @@ void load_kernel(EFI_HANDLE image_handle) {
 
   if (status != EFI_SUCCESS) {
     status_msg(status, __FILE__, __func__, __LINE__);
+    return NULL;
   }
 
-  // AllocatePool
+  return file_handle;
+}
+
+UINT64 get_file_size(EFI_FILE_HANDLE file_handle) {
+  // make sure file_handle is valid before calling, because this function has no
+  // way to return errors
+
   EFI_FILE_INFO *fi;
+
   fi = LibFileInfo(file_handle);
   UINT64 file_size = fi->FileSize;
-  Print(L"File size: %u\r\n", file_size);
   FreePool(fi);
+
+  return file_size;
+}
+
+void load_kernel(EFI_HANDLE image_handle) {
+
+  EFI_LOADED_IMAGE *loaded_image;
+  EFI_FILE_HANDLE volume_handle, file_handle;
+  EFI_STATUS status;
+
+  status = get_loaded_image(image_handle, &loaded_image);
+  if (status != EFI_SUCCESS) {
+    status_msg(status, __FILE__, __func__, __LINE__);
+    return;
+  }
+
+  volume_handle = get_volume_handle(loaded_image);
+
+  CHAR16 *path = u"\\EFI\\BOOT\\test.txt";
+  file_handle = open_file(volume_handle, path);
+
+  UINT64 file_size = get_file_size(file_handle);
 
   char *buf;
   buf = AllocatePool(file_size + 1);
