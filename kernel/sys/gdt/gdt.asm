123871a2368cec84e86828d9438a26005fc873bf
Kurt M. Weber <kurt.maxwell.weber@gmail.com>
2026-02-09 19:20:56 -0600

    fixing some screenfont errors

--- kernel/sys/gdt/gdt.asm
+++ kernel/sys/gdt/gdt.asm
@@ -42,7 +42,7 @@ GDT64:                           ; Global Descriptor Table (64-bit).
     dw 0                         ; Base (low).
     db 0                         ; Base (middle)
     db 10011010b                 ; Access (exec/read). (0x9A)
-    db 10101111b                 ; Granularity, 64 bits flag, limit19:16.
+    db 10101111b                 ; Granularity, 64 bits flag, limit 19:16.
     db 0                         ; Base (high).
     .KernelData: equ $ - GDT64   ; The kernel data descriptor.
     dw 0                         ; Limit (low).
@@ -56,21 +56,21 @@ GDT64:                           ; Global Descriptor Table (64-bit).
     dw 0                         ; Base (low).
     db 0                         ; Base (middle)
     db 11111010b                 ; Access (exec/read). (0xFA)
-    db 10101111b                 ; Granularity, 64 bits flag, limit19:16.
-    db 0                         ; Base (high).
-    .UserData: equ $ - GDT64     ; The user data descriptor.
-    dw 0                         ; Limit (low).
-    dw 0                         ; Base (low).
+    db 10101111b                 ; Granularity, 64 bits flag, limit 19:16
+    db 0                         ; Base (high)
+    .UserData: equ $ - GDT64     ; The user data descriptor
+    dw 0                         ; Limit (low)
+    dw 0                         ; Base (low)
     db 0                         ; Base (middle)
-    db 11110010b                 ; Access (read/write). (0xF2)
-    db 00000000b                 ; Granularity.
-    db 0                         ; Base (high).
+    db 11110010b                 ; Access (read/write) (0xF2)
+    db 00000000b                 ; Granularity
+    db 0                         ; Base (high)
 	.tss1: equ $ - GDT64
 	dq 0
 	.tss2: equ $ - GDT64
 	dq 0
-    .Pointer:                    ; The GDT-pointer.
-    dw $ - GDT64 - 1             ; Limit.
-    dq GDT64                     ; Base.
+    .Pointer:                    ; GDT pointer
+    dw $ - GDT64 - 1             ; Limit
+    dq GDT64                     ; Base
 
     section .note.GNU-stack noalloc noexec nowrite progbits
